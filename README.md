692d6fcf9ea9442d6cc60cce0d24d78c9a39c064
Greg <lupasco.grigori@gmail.com>
2022-02-03 20:23:59 +0200

    Update README

---
+++ README.md
@@ -0,0 +1,50 @@
+# Bigger Number App
+
+## *Gregory L.*
+
+**Bigger Number App** prompts the user to select which of the tow numbers presented is larger. The background is updated depending on if the correct answer is chose. New numbers
+are randomly selected after a user selection.
+
+Time spent: **1.5** hours spent in total
+
+## Functionality
+
+The following **required** functionality is completed:
+
+* [x] User can select one of tow button options shown
+* [x] The user sees the background color change based on the number (button) selected
+* [x] The user sees new distinct numbers displayed on the button after either button is tapped
+
+The following **extensions** are implemented:
+
+* [X] Custom colors palette selected
+* [ ] Change the main theme colors
+* [ ] Custom button colors
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright [2022] [Gregory L]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ app/src/main/java/com/example/chosebiggernumber/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.chosebiggernumber
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        assignNumbersToButtons()
+        buttonLeft.setOnClickListener {
+            answerCheck(true)
+            assignNumbersToButtons()
+        }
+
+        buttonRight.setOnClickListener {
+            answerCheck(false)
+            assignNumbersToButtons()
+        }
+    }
+
+    private fun answerCheck(isLeftButtonSelected: Boolean){
+        val leftNum = buttonLeft.text.toString().toInt()
+        val rightNum = buttonRight.text.toString().toInt()
+        val isAnswerCorrect : Boolean = if (isLeftButtonSelected) leftNum > rightNum else rightNum > leftNum
+        if (isAnswerCorrect) {
+            // Change the background colour
+            backgroundView.setBackgroundColor(Color.GREEN)
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show()
+        } else {
+            backgroundView.setBackgroundColor(Color.RED)
+            Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun assignNumbersToButtons() {
+        // Calling the Random function
+        val r = Random
+        val leftNum : Int = r.nextInt(99)
+        var rightNum : Int = leftNum
+        while (rightNum == leftNum){
+            rightNum = r.nextInt(99)
+        }
+        buttonLeft.text = leftNum.toString()
+        buttonRight.text = rightNum.toString()
+    }
+}
